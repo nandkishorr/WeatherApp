bbb75667927e70d8f6bfb2a6a8529c1ff8bd6899
nandkishorr <rachulr7@gmail.com>
2024-10-21 06:10:06 +0530

    db connected

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require("morgan");
+const dotenv = require('dotenv');
+const connectToDb =require('./config/Connection') 
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 3000;
+// Middleware
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+connectToDb()
+
+// Define your routes here
+// const Routes = require('./routes');
+// app.use('/', Routes);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ src/config/Connection.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+async function connectToDb() {
+    try {
+        // Load environment variables
+        dotenv.config();
+        
+        // Get MongoDB URI from environment variables
+        const dbUri = process.env.MONGODB_URL;
+
+        // Connect to MongoDB
+       const connection = await mongoose.connect(dbUri, {
+            dbName: '', 
+        });
+        console.log('Connected to MongoDB ');
+        return connection;
+    } catch (error) {
+        console.error('Error connecting to MongoDB', error);
+        // You might want to handle the error further, depending on your application's requirements
+    }
+}
+
+// Close MongoDB connection when the Node.js process terminates
+process.on('SIGINT', async () => {
+    try {
+        await mongoose.connection.close();
+        console.log('MongoDB connection closed');
+        process.exit(0);
+    } catch (error) {
+        console.error('Error closing MongoDB connection', error);
+        process.exit(1);
+    }
+});
+
+module.exports = connectToDb;
