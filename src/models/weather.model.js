a2aae2911be356a07b99bdb8bcd9f2101bff21dc
nandkishorr <rachulr7@gmail.com>
2024-10-22 15:53:41 +0530

    weather model create

---
+++ src/models/weather.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const weatherSchema = new mongoose.Schema({
+        city: { type: String, required: true },
+        temperature: { type: Number, required: true },
+        feels_like: { type: Number, required: true },
+        condition: { type: String, required: true },
+        timestamp: { type: Date, default: Date.now }, 
+      });
+      
+const Weather = mongoose.model('weatherSchema', weatherSchema);
+module.exports = Weather;
